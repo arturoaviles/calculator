75d2f08838d05027a67343dee6f3814d339dcd3a
Arturo Avilés <aaviles@nearshoremx.com>
2018-09-07 17:07:06 -0500

    Change receiving param type from variadic func with numbers to slice of ints

--- calculator.go
+++ calculator.go
@@ -1,7 +1,7 @@
 package calculator
 
 // Calculate sums all the numbers passed to it.
-func Calculate(numbers ...int) int {
+func Calculate(numbers []int) int {
 	total := 0
 	for _, number := range numbers {
 		total += number
